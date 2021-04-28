70f1fa39afa69f6e539a131fc8b84f2a69b8feca
alekseishkel <lexashkel@gmail.com>
2021-04-28 19:08:45 +0300

    Updates

---
+++ precache-manifest.2102406e35504b1e840d4fb1cd4505e9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f1d73a2c0595a0eb0a94118fb84653ee",
+    "url": "/star-db/index.html"
+  },
+  {
+    "revision": "8a7c6d11d64f3b7a6804",
+    "url": "/star-db/static/css/main.fec18a9e.chunk.css"
+  },
+  {
+    "revision": "54af02626a3463843363",
+    "url": "/star-db/static/js/2.667197f9.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/star-db/static/js/2.667197f9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8a7c6d11d64f3b7a6804",
+    "url": "/star-db/static/js/main.db6a8e50.chunk.js"
+  },
+  {
+    "revision": "5e9fe22e0f74b24b7463",
+    "url": "/star-db/static/js/runtime-main.d9f21290.js"
+  }
+]);
